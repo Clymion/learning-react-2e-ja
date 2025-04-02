fce8dde72faa8ab9666dfee10a1f30d9242ccf1c
Clymion <tech2subus@gmail.com>
2025-04-02 13:27:40 +0000

    6.4 `index.tsx`にプラバイダ―を直接定義するサンプル

--- chapter-06/6.1/01/src/index.tsx
+++ chapter-06/6.1/01/src/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { createContext } from "react";
 import { createRoot } from "react-dom/client";
+import colors from './color-data.json';
 import App from "./App";
 
+export type ColorType = typeof colors;
+export const ColorContext = createContext<ColorType | undefined>(undefined);
 const root = createRoot(document.getElementById("root")!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorContext.Provider value={{ colors }}>
+      <App />
+    </ColorContext.Provider>
   </React.StrictMode>,
 );
