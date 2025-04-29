9f75005af3327763571e31a1c1e0e9e9872ed235
Clymion <tech2subus@gmail.com>
2025-04-29 07:40:43 +0000

    非効率なリスト描画の実装

--- chapter-08/8.4/01/src/App.tsx
+++ chapter-08/8.4/01/src/App.tsx
@@ -1,58 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
-type GitHubUserProps = {
-  login: string;
-  name: string;
-  location: string;
-  avatar_url: string;
-};
+const tahoe_peaks = [
+  { name: 'Freel Peak', elevation: 10891 },
+  { name: 'Monument Peak', elevation: 10067 },
+  { name: 'Pyramid Peak', elevation: 9983 },
+  { name: 'Mt. Tallac', elevation: 9735 },
+];
 
-const LoginUserForm = () => {
-  const [login, setLogin] = useState('moonhighway');
-  return (
-    <>
-      <h1>Login User Form</h1>
-      <input
-        type="text"
-        defaultValue={login}
-        onBlur={(e) => setLogin(e.target.value)}
-      />
-      <GitHubUser login={login} />
-    </>
-  );
-};
-
-function GitHubUser({ login }) {
-  const [data, setData] = useState<GitHubUserProps>();
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then(data => data.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (loading) return <h1>loading...</h1>;
-  if (!data) return null;
-
-  return (
-    <div className="githubUser">
-      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
-      <div>
-        <h1>{data.login}</h1>
-        {data.name && <p>{data.name}</p>}
-        {data.location && <p>{data.location}</p>}
-      </div>
-    </div>
+function List({
+  data = [],
+  renderItem,
+  renderEmpty,
+}: {
+  data: any[];
+  renderItem: (item: any) => React.ReactNode;
+  renderEmpty: React.ReactNode;
+}) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 export default function App() {
-  return <LoginUserForm />;
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}
+        </>
+      )}
+    />
+  );
 }
