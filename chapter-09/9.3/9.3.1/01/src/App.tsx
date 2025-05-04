5b49e5b2e983bd026b8d8f1402e2887700646ac5
clymion <tech2subus@gmail.com>
2025-05-04 04:33:43 +0000

    Promiseとエラーのthrow、resolveの処理の共通化（サスペンスデータソース）

--- chapter-09/9.3/9.3.1/01/src/App.tsx
+++ chapter-09/9.3/9.3.1/01/src/App.tsx
@@ -1,17 +1,40 @@
 import React, { Suspense } from 'react';
 import ErrorBoundary from './ErrorBoundary';
+import GridLoader from 'react-spinners/GridLoader';
 
-const loadStatus = () => 'success - ready';
+function createResource(pending) {
+  let error, response;
+  pending.then((r) => (response = r)).catch((e) => (error = e));
+  return {
+    read() {
+      if (error) throw error;
+      if (response) return response;
+      throw pending;
+    },
+  };
+}
 
-function Status() {
-  const status = loadStatus();
-  return <h1>status: {status}</h1>;
+const threeSecondsToGnar = new Promise((resolves) =>
+  setTimeout(() => resolves({ gnar: 'gnarly!' }), 3000)
+);
+
+const sampleError = new Promise((_, rejects) =>
+  setTimeout(() => rejects(new Error('Gnar error!')), 3000)
+);
+
+const resource = createResource(sampleError);
+
+function Gnar() {
+  const result = resource.read();
+  return <h1>Gnar: {result.gnar}</h1>;
 }
 
 export default function App() {
   return (
-    <ErrorBoundary>
-      <Status />
-    </ErrorBoundary>
+    <Suspense fallback={<GridLoader />}>
+      <ErrorBoundary>
+        <Gnar />
+      </ErrorBoundary>
+    </Suspense>
   );
 }
